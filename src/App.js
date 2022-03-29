2a77ed7c832cddf1fb180e085ab9bffce5adeee0
Mihail Voevodin <mrfirst3333@gmail.com>
2022-03-29 21:57:20 +0300

    add error and some styles

--- src/App.js
+++ src/App.js
@@ -17,26 +17,41 @@ class App extends React.Component {
         wind: undefined,
         pressure: undefined,
         humidity: undefined,
+        error: undefined
     };
 
     getCurrentWeather = async (e) => {
         e.preventDefault();
-        
-        
         const city = e.target.elements.city.value;
-        const apiUrl = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
-        const data = await apiUrl.json();
-        console.log(data);
+        
+        if (city) {
+            const apiUrl = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+            const data = await apiUrl.json();
+            console.log(data);
 
-        this.setState({
-            temp: data.main.temp,
-            city: data.name,
-            feels_like: data.main.feels_like,
-            main: data.weather[0].main,
-            wind: data.wind.speed,
-            pressure: data.main.pressure,
-            humidity: data.main.humidity,
-        })
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                feels_like: data.main.feels_like,
+                main: data.weather[0].main,
+                wind: data.wind.speed,
+                pressure: data.main.pressure,
+                humidity: data.main.humidity,
+                error: undefined,
+            }) 
+        } else {
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                feels_like: undefined,
+                main: undefined,
+                wind: undefined,
+                pressure: undefined,
+                humidity: undefined,
+                error: 'Введите город',
+            }) 
+        }
+       
     }
 
     render() {
